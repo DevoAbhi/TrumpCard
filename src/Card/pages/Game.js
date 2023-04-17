ecde11ebb25ea15459990a2c5e65c0ce3f3be82c
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-17 17:52:50 +0530

    create the game component and make the cards appear for both the players

---
+++ src/Card/pages/Game.js
@@ -0,0 +1,203 @@
+import { useLocation } from 'react-router-dom';
+import Card from './Card';
+import './Game.css';
+
+const Game = () => {
+    const location = useLocation();
+    const cardType = new URLSearchParams(location.search).get('cardType');
+    console.log(cardType);
+
+
+    const player1Deck = [
+        {
+            "_id": ("643ab5612b863a1858059216"),
+            "cardType": "pokemon",
+            "cardName": "reshiram",
+            "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/643.png",
+            "attributes": [
+                {
+                    "name": "hp",
+                    "value": "100",
+                    "_id": ("643ab5612b863a1858059217")
+                },
+                {
+                    "name": "attack",
+                    "value": "120",
+                    "_id": ("643ab5612b863a1858059218")
+                },
+                {
+                    "name": "defense",
+                    "value": "100",
+                    "_id": ("643ab5612b863a1858059219")
+                },
+                {
+                    "name": "special-attack",
+                    "value": "150",
+                    "_id": ("643ab5612b863a185805921a")
+                },
+                {
+                    "name": "special-defense",
+                    "value": "120",
+                    "_id": ("643ab5612b863a185805921b")
+                },
+                {
+                    "name": "speed",
+                    "value": "90",
+                    "_id": ("643ab5612b863a185805921c")
+                }
+            ],
+            "__v": (0)
+        }
+
+        , {
+            "_id": ("643ab55f2b863a18580591fe"),
+            "cardType": "pokemon",
+            "cardName": "palkia",
+            "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/484.png",
+            "attributes": [
+                {
+                    "name": "hp",
+                    "value": "90",
+                    "_id": ("643ab55f2b863a18580591ff")
+                },
+                {
+                    "name": "attack",
+                    "value": "120",
+                    "_id": ("643ab55f2b863a1858059200")
+                },
+                {
+                    "name": "defense",
+                    "value": "100",
+                    "_id": ("643ab55f2b863a1858059201")
+                },
+                {
+                    "name": "special-attack",
+                    "value": "150",
+                    "_id": ("643ab55f2b863a1858059202")
+                },
+                {
+                    "name": "special-defense",
+                    "value": "120",
+                    "_id": ("643ab55f2b863a1858059203")
+                },
+                {
+                    "name": "speed",
+                    "value": "100",
+                    "_id": ("643ab55f2b863a1858059204")
+                }
+            ],
+            "__v": (0)
+        }
+    ]
+
+    const player2Deck = [
+        {
+            "_id": ("643ab5572b863a18580590c6"),
+            "cardType": "pokemon",
+            "cardName": "blastoise",
+            "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+            "attributes": [
+                {
+                    "name": "hp",
+                    "value": "79",
+                    "_id": ("643ab5572b863a18580590c7")
+                },
+                {
+                    "name": "attack",
+                    "value": "83",
+                    "_id": ("643ab5572b863a18580590c8")
+                },
+                {
+                    "name": "defense",
+                    "value": "100",
+                    "_id": ("643ab5572b863a18580590c9")
+                },
+                {
+                    "name": "special-attack",
+                    "value": "85",
+                    "_id": ("643ab5572b863a18580590ca")
+                },
+                {
+                    "name": "special-defense",
+                    "value": "105",
+                    "_id": ("643ab5572b863a18580590cb")
+                },
+                {
+                    "name": "speed",
+                    "value": "78",
+                    "_id": ("643ab5572b863a18580590cc")
+                }
+            ],
+            "__v": (0)
+        }
+
+        , {
+            "cardType": "pokemon",
+            "cardName": "mewtwo",
+            "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
+            "attributes": [
+                {
+                    "name": "hp",
+                    "value": "106",
+                    "_id": ("643ab5572b863a18580590bf")
+                },
+                {
+                    "name": "attack",
+                    "value": "110",
+                    "_id": ("643ab5572b863a18580590c0")
+                },
+                {
+                    "name": "defense",
+                    "value": "90",
+                    "_id": ("643ab5572b863a18580590c1")
+                },
+                {
+                    "name": "special-attack",
+                    "value": "154",
+                    "_id": ("643ab5572b863a18580590c2")
+                },
+                {
+                    "name": "special-def",
+                    "value": "90",
+                    "_id": ("643ab5572b863a18580590c3")
+                },
+                {
+                    "name": "speed",
+                    "value": "130",
+                    "_id": ("643ab5572b863a18580590c4")
+                }
+            ],
+            "__v": 0
+        }
+    ]
+
+    return (
+        <div className='game'>
+            <div className="player1Area">
+                <div className="cards1">
+                    {player1Deck.map(card => (
+                        <Card
+                            name={card.name}
+                            imageUrl={card.imageUrl}
+
+                        ></Card>
+                    ))}
+                </div>
+            </div>
+            <div className="gameArea"></div>
+            <div className="player2Area">
+            <div className="cards2">
+                    {player2Deck.map(card => (
+                        <Card
+                            name={card.name}
+                            imageUrl={card.imageUrl}
+
+                        ></Card>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Game;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,17 +1,66 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './Home.css';
+
+// const Home = () => {
+//   return (
+//     <div className="home-container">
+//       <h2 className="home-title">Welcome to the Trump Card Game!</h2>
+//       <p className="home-description">Click the button below to start playing.</p>
+//       <Link to="/cards">
+//         <button className="home-button">Start Game</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
-const Home = () => {
+const PokemonCard = ({ name, image, description, hp, attack, defense, specialAttack, specialDefense, speed }) => {
   return (
-    <div className="home-container">
-      <h2 className="home-title">Welcome to the Trump Card Game!</h2>
-      <p className="home-description">Click the button below to start playing.</p>
-      <Link to="/cards">
-        <button className="home-button">Start Game</button>
-      </Link>
+    <div className="pokemon-card">
+      <div className="pokemon-card-image-container">
+        <img className="pokemon-card-image" src={image} alt={name} />
+        <div className="pokemon-card-description">Lorem ipsum dolor sit amet</div>
+      </div>
+      <div className="pokemon-card-details">
+        <h2 className="pokemon-card-name">ABhinab</h2>
+        <div className="pokemon-card-attributes-container">
+          <div className="pokemon-card-attributes-column">
+            <div className="pokemon-card-attribute">
+              <div className="pokemon-card-attribute-name">HP:</div>
+              <div className="pokemon-card-attribute-value">20</div>
+            </div>
+            <div className="pokemon-card-attribute">
+              <div className="pokemon-card-attribute-name">Attack:</div>
+              <div className="pokemon-card-attribute-value">20</div>
+            </div>
+            <div className="pokemon-card-attribute">
+              <div className="pokemon-card-attribute-name">Defense:</div>
+              <div className="pokemon-card-attribute-value">20</div>
+            </div>
+          </div>
+          <div className="pokemon-card-attributes-column">
+            <div className="pokemon-card-attribute">
+              <div className="pokemon-card-attribute-name">Special Attack:</div>
+              <div className="pokemon-card-attribute-value">20</div>
+            </div>
+            <div className="pokemon-card-attribute">
+              <div className="pokemon-card-attribute-name">Special:</div>
+              <div className="pokemon-card-attribute-value">20</div>
+            </div>
+            <div className="pokemon-card-attribute">
+              <div className="pokemon-card-attribute-name">Speed:</div>
+              <div className="pokemon-card-attribute-value">20</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default PokemonCard;
