2440dd9b7f3fe886ea4160d1117f12144f4177ad
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-20 02:20:51 +0530

    complete the modal component

--- src/Card/components/Modal.js
+++ src/Card/components/Modal.js
@@ -26,17 +26,18 @@ function Modal(props) {
                     </div>
                 }
 
+<button className="closeBtn" onClick={handleCloseModal}>Continue</button>
+
                 <div className="reason">
                     {!props.isTie &&
                         <div className="winner-margin">
-
                             <span className='winner-name'>Abhinab</span> has won by a margin of <span className='margin'>{props.margin}</span>!
                         </div>
                     }
 
                     {/* <span className='winner-name'>{props.winner.name}</span> has won by a margin of <span className='margin'>{props.margin}</span>! */}
                     <div className='both-values'>
-                        <span className='value1'>Abhinab = {props.winner.attributes.find(attribute => attribute.name === props.attributeChosen)?.value}</span>
+                        <span className='value1'>Abhinab = {props.winner.attributes.find(attribute => attribute.name === props.attributeChosen)?.value} </span>
                         {/* <span className='value1'>{props.winner.name} = {props.winner.attributes.find(attribute => attribute.name === props.attributeChosen)?.value}</span> */}
                         <span className='value2'>Abhinab = {props.loser.attributes.find(attribute => attribute.name === props.attributeChosen)?.value}</span>
                         {/* <span className='value2'>{props.loser.name} = {props.loser.attributes.find(attribute => attribute.name === props.attributeChosen)?.value}</span> */}
