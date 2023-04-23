b9d05b48d42c9c96628de64f194746b6cac9d3d9
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-23 17:18:26 +0530

    change the file to adapt to new react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Home/Home';
 import Navbar from './Navbar/Navbar';
 import Game from './Card/pages/Game';
 import Card from './Card/pages/Card';
+import Auth from './Auth/pages/Auth';
+import Dashboard from './Dashboard/pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <Navbar></Navbar>
-      <Switch>
-      <Route path="/" exact >
-      <Home></Home>
-      </Route>
-      <Route path="/game" exact >
-        <Game></Game>
-      </Route>
-      <Route path="/card" exact >
-        <Card></Card>
-      </Route>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
 
-      </Switch>
+        <Route path="/game" exact element={<Game />} />
+
+        <Route path="/card" exact element={<Card />} />
+
+        <Route path="/auth" exact element={<Auth />} />
+
+        <Route path='/dashboard' exact element={<Dashboard />}/>
+      </Routes>
     </Router>
   );
 }
