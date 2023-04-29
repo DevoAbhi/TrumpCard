41ef79f7adf743b3221d2d1ac086e9faa890a1b3
AbhiDroid <abhinabroy2001@gmail.com>
2023-04-29 17:18:24 +0530

    implements score action in game component

--- src/Card/pages/Game.js
+++ src/Card/pages/Game.js
@@ -2,212 +2,10 @@ import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from './Card';
+import Score from '../components/Score';
 import GameSection from '../components/GameSection';
 import { getCards } from '../../stateManager/action-creators';
-// const player1DeckCards = [
-//     // {
-//     //     "_id" : ("643aee2483e91332c703146b"),
-//     //     "cardType" : "avengers",
-//     //     "cardName" : "thor",
-//     //     "imageUrl" : "https://www.superherodb.com/pictures2/portraits/10/100/727.jpg",
-//     //     "attributes" : [
-//     //         {
-//     //             "name" : "intelligence",
-//     //             "value" : "100",
-//     //             "_id" : ("643aee2483e91332c703146c")
-//     //         },
-//     //         {
-//     //             "name" : "strength",
-//     //             "value" : "53",
-//     //             "_id" : ("643aee2483e91332c703146d")
-//     //         },
-//     //         {
-//     //             "name" : "speed",
-//     //             "value" : "25",
-//     //             "_id" : ("643aee2483e91332c703146e")
-//     //         },
-//     //         {
-//     //             "name" : "durability",
-//     //             "value" : "65",
-//     //             "_id" : ("643aee2483e91332c703146f")
-//     //         },
-//     //         {
-//     //             "name" : "power",
-//     //             "value" : "68",
-//     //             "_id" : ("643aee2483e91332c7031470")
-//     //         },
-//     //         {
-//     //             "name" : "combat",
-//     //             "value" : "70",
-//     //             "_id" : ("643aee2483e91332c7031471")
-//     //         }
-//     //     ],
-//     //     "__v" : (0)
-//     // }
-
-//     {
-//         "_id": "643ab5612b863a1858059216",
-//         "cardType": "pokemon",
-//         "cardName": "reshiram",
-//         "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/643.png",
-//         "attributes": [
-//             {
-//                 "name": "hp",
-//                 "value": "100",
-//                 "_id": ("643ab5612b863a1858059217")
-//             },
-//             {
-//                 "name": "attack",
-//                 "value": "120",
-//                 "_id": ("643ab5612b863a1858059218")
-//             },
-//             {
-//                 "name": "defense",
-//                 "value": "100",
-//                 "_id": ("643ab5612b863a1858059219")
-//             },
-//             {
-//                 "name": "special-attack",
-//                 "value": "150",
-//                 "_id": ("643ab5612b863a185805921a")
-//             },
-//             {
-//                 "name": "special-defense",
-//                 "value": "120",
-//                 "_id": ("643ab5612b863a185805921b")
-//             },
-//             {
-//                 "name": "speed",
-//                 "value": "90",
-//                 "_id": ("643ab5612b863a185805921c")
-//             }
-//         ],
-//         "__v": (0)
-//     }
-
-//     , {
-//         "_id": "643ab55f2b863a18580591fe",
-//         "cardType": "pokemon",
-//         "cardName": "palkia",
-//         "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/484.png",
-//         "attributes": [
-//             {
-//                 "name": "hp",
-//                 "value": "90",
-//                 "_id": ("643ab55f2b863a18580591ff")
-//             },
-//             {
-//                 "name": "attack",
-//                 "value": "120",
-//                 "_id": ("643ab55f2b863a1858059200")
-//             },
-//             {
-//                 "name": "defense",
-//                 "value": "100",
-//                 "_id": ("643ab55f2b863a1858059201")
-//             },
-//             {
-//                 "name": "special-attack",
-//                 "value": "150",
-//                 "_id": ("643ab55f2b863a1858059202")
-//             },
-//             {
-//                 "name": "special-defense",
-//                 "value": "120",
-//                 "_id": ("643ab55f2b863a1858059203")
-//             },
-//             {
-//                 "name": "speed",
-//                 "value": "100",
-//                 "_id": ("643ab55f2b863a1858059204")
-//             }
-//         ],
-//         "__v": (0)
-//     }
-// ]
-
-// const player2DeckCards = [
-//     {
-//         "_id": "643ab5572b863a18580590c6",
-//         "cardType": "pokemon",
-//         "cardName": "blastoise",
-//         "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
-//         "attributes": [
-//             {
-//                 "name": "hp",
-//                 "value": "79",
-//                 "_id": ("643ab5572b863a18580590c7")
-//             },
-//             {
-//                 "name": "attack",
-//                 "value": "83",
-//                 "_id": ("643ab5572b863a18580590c8")
-//             },
-//             {
-//                 "name": "defense",
-//                 "value": "100",
-//                 "_id": ("643ab5572b863a18580590c9")
-//             },
-//             {
-//                 "name": "special-attack",
-//                 "value": "85",
-//                 "_id": ("643ab5572b863a18580590ca")
-//             },
-//             {
-//                 "name": "special-defense",
-//                 "value": "105",
-//                 "_id": ("643ab5572b863a18580590cb")
-//             },
-//             {
-//                 "name": "speed",
-//                 "value": "78",
-//                 "_id": ("643ab5572b863a18580590cc")
-//             }
-//         ],
-//         "__v": (0)
-//     }
-
-//     , {
-//         "_id": "643ab5572b863a18580590c8",
-//         "cardType": "pokemon",
-//         "cardName": "mewtwo",
-//         "imageUrl": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png",
-//         "attributes": [
-//             {
-//                 "name": "hp",
-//                 "value": "106",
-//                 "_id": ("643ab5572b863a18580590bf")
-//             },
-//             {
-//                 "name": "attack",
-//                 "value": "110",
-//                 "_id": ("643ab5572b863a18580590c0")
-//             },
-//             {
-//                 "name": "defense",
-//                 "value": "90",
-//                 "_id": ("643ab5572b863a18580590c1")
-//             },
-//             {
-//                 "name": "special-attack",
-//                 "value": "154",
-//                 "_id": ("643ab5572b863a18580590c2")
-//             },
-//             {
-//                 "name": "special-def",
-//                 "value": "90",
-//                 "_id": ("643ab5572b863a18580590c3")
-//             },
-//             {
-//                 "name": "speed",
-//                 "value": "130",
-//                 "_id": ("643ab5572b863a18580590c4")
-//             }
-//         ],
-//         "__v": 0
-//     }
-// ]
-
+import { updateScore } from '../../stateManager/action-creators';
 
 import './Game.css';
 
@@ -216,6 +14,7 @@ const Game = () => {
     const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('profile')));
     const dispatch = useDispatch();
     const playerCards = useSelector((state) => state.card);
+    const playerScores = useSelector((state) => state.score);
     const location = useLocation();
     const cardType = new URLSearchParams(location.search).get('cardType');
     // const [loading, setLoading] = useState(true);
@@ -224,6 +23,7 @@ const Game = () => {
 
     useEffect(() => {
         dispatch(getCards(cardType))
+        dispatch(updateScore("SET TO ZERO", 0));
     }, [dispatch, cardType])
 
     useEffect(() => {
@@ -236,26 +36,16 @@ const Game = () => {
         */
 
         setUserData(JSON.parse(localStorage.getItem('profile')))
-      }, [location]);
+    }, [location]);
 
-    // useEffect(() => {
-    //     setPlayer1Deck(playerCards.player1Deck);
-    //     setPlayer2Deck(playerCards.player2Deck);
 
-    // }, [playerCards])
 
     const player1Deck = playerCards.player1Deck;
     const player2Deck = playerCards.player2Deck;
 
     const [player1ChosenCard, setPlayer1ChosenCard] = useState({});
     const [player2ChosenCard, setPlayer2ChosenCard] = useState({});
-    // const [player1Deck, setPlayer1Deck] = useState(playerCards.player1Deck);
-    // const [player2Deck, setPlayer2Deck] = useState(playerCards.player2Deck);
-
-
-    // if(loading){
-    //     return <div>loading...</div>
-    // }
+    const [margin, setMargin] = useState({ winner: "", score: 0 });
 
     return (
         <div className='game' >
@@ -287,6 +77,10 @@ const Game = () => {
                 </div>
                 <div className="name1">{userData.user.username}</div>
 
+                <Score>
+                    Score : {playerScores.player1Score}
+                </Score>
+
             </div>
 
 
@@ -295,8 +89,10 @@ const Game = () => {
                 player2ChosenCard={player2ChosenCard}
                 setPlayer1ChosenCard={setPlayer1ChosenCard}
                 setPlayer2ChosenCard={setPlayer2ChosenCard}
-                player1 = {userData.user.username}
-                player2 = "Computer"
+                player1={userData.user.username}
+                player2="Computer"
+                {...margin}
+                setMargin={setMargin}
             ></GameSection>
 
 
@@ -327,6 +123,9 @@ const Game = () => {
                     ))}
                 </div>
                 <div className="name2">Computer</div>
+                <Score>
+                    Score : {playerScores.player2Score}
+                </Score>
             </div>
         </div>
     )
